4a028884704eb6859dbef452d825187753773363
iPoolito <poolortegas@gmail.com>
2022-12-20 17:51:35 -0600

    Addinf fire base to our proyect

--- src/components/CicloDeVida.jsx
+++ src/components/CicloDeVida.jsx
@@ -1,11 +1,14 @@
-import { useContext } from "react"
+import { useContext, useEffect, useState } from "react"
 import cartContext from "../context/cartContext"
 import Context from "../context/AuthContext/authContext"
-
+import { getFirestore, doc, getDoc, collection, getDocs, query, where } from 'firebase/firestore'
 export default function CicloDeVida() {
 
     // const data = useContext(cartContext)
     const { isLoggedin, onLogin, onLogout } = useContext(Context)
+    const [product, setProduct] = useState()
+    const [products, setProducts] = useState()
+    const [queryProducts, setQueryProducts] = useState()
 
     const clickHandler = () => {
         if (isLoggedin) {
@@ -14,6 +17,35 @@ export default function CicloDeVida() {
             onLogin()
         }
     }
+    useEffect(() => {
+        const db = getFirestore()
+        const itemRef = doc(db, 'productos', "BU4XFk0uww1K8yjOgvBV")
+        getDoc(itemRef).then((snapshot) => {
+            if (snapshot.exists()) {
+                setProduct({ id: snapshot.id, ...snapshot.data() })
+            }
+        })
+        //Consult without filters
+        const itemsCollection = collection(db, 'productos')
+        getDocs(itemsCollection).then(snapshot => {
+            setProducts(snapshot.docs.map((doc) => {
+                return { id: doc.id, ...doc.data() }
+            }))
+        })
+
+        //Consult with filters
+
+        const q = query(collection(db, "productos"), where("price", ">", 99))
+        getDocs(q).then((snapshot) => {
+            if (snapshot.size === 0) {
+                console.log('No results')
+            }
+            setQueryProducts(snapshot.docs.map((doc) => {
+                return { id: doc.id, ...doc.data() }
+            }))
+        })
+    }, [])
+    console.log(product, products, queryProducts)
     return (
         <div>
             <h1>Hola!{isLoggedin ? 'Estas conectado' : 'No estas Conectado'}</h1>
--- src/index.js
+++ src/index.js
@@ -3,6 +3,23 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyDFa5oWBKyO76mbyygXLlE1MH55U5Uufzo",
+  authDomain: "coder-react-dd349.firebaseapp.com",
+  projectId: "coder-react-dd349",
+  storageBucket: "coder-react-dd349.appspot.com",
+  messagingSenderId: "141527067218",
+  appId: "1:141527067218:web:5d7beab726ebaca9d1cd8b"
+};
+
+// Initialize Firebase
+initializeApp(firebaseConfig);
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
